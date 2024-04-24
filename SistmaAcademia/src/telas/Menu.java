16b296991ab9813524d39faad4c2550bd90050e6
brenocodess <168038934+brenocodess@users.noreply.github.com>
2024-04-24 20:39:42 -0300

    ultima dia 24
    abril

--- SistmaAcademia/src/telas/Menu.java
+++ SistmaAcademia/src/telas/Menu.java
@@ -32,13 +32,14 @@ public class Menu extends javax.swing.JFrame {
         Azul = new javax.swing.JMenuItem();
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenu6 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
         jMenu4 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Janela Menu");
 
-        jMenu1.setText("Cadastro");
+        jMenu1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Produtos.png"))); // NOI18N
+        jMenu1.setText("Produtos");
 
         jMenu5.setText("Camisetas");
 
@@ -60,9 +61,7 @@ public class Menu extends javax.swing.JFrame {
 
         MenuPrincipal.add(jMenu1);
 
-        jMenu2.setText("Aparelhos");
-        MenuPrincipal.add(jMenu2);
-
+        jMenu3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/money.png"))); // NOI18N
         jMenu3.setText("Financeiro");
         MenuPrincipal.add(jMenu3);
 
@@ -75,11 +74,11 @@ public class Menu extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 789, Short.MAX_VALUE)
+            .addGap(0, 558, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 273, Short.MAX_VALUE)
+            .addGap(0, 96, Short.MAX_VALUE)
         );
 
         pack();
@@ -128,7 +127,6 @@ public class Menu extends javax.swing.JFrame {
     private javax.swing.JMenuItem Azul;
     private javax.swing.JMenuBar MenuPrincipal;
     private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenu jMenu5;
